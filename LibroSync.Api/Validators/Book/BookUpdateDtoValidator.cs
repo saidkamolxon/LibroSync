215a5204016e71f2e2ad1b4b041fd08b03aa9e03
Saidkamolxon <saidkamolxon1997@gmail.com>
2025-02-23 20:44:53 +0500

    Validators added

--- LibroSync.Api/Validators/Book/BookUpdateDtoValidator.cs
+++ LibroSync.Api/Validators/Book/BookUpdateDtoValidator.cs
@@ -1,8 +1,33 @@
 ﻿using FluentValidation;
 using LibroSync.Api.DTOs.Book;
+using LibroSync.Data.IRepositories;
 
 namespace LibroSync.Api.Validators.Book;
 
 public class BookUpdateDtoValidator : AbstractValidator<BookUpdateDto>
 {
+    public BookUpdateDtoValidator(IServiceProvider serviceProvider)
+    {
+        RuleFor(b => b.Id)
+            .NotEmpty().WithMessage("Book ID is required.")
+            .GreaterThan(0).WithMessage("Book ID must be greater than 0.");
+
+        RuleFor(b => b.Title)
+            .NotEmpty().WithMessage("Updated book title is required.")
+            .Length(3, 100).WithMessage("Updated book title must be between 3 and 100 characters.")
+            .MustAsync(async (title, cancellationToken) =>
+            {
+                var repository = serviceProvider.GetRequiredService<IRepository>();
+                return await repository.RetrieveByTitleAsync(title, cancellationToken) is null;
+            })
+            .WithMessage("Another book with this title already exists.");
+
+        RuleFor(b => b.AuthorName)
+            .NotEmpty().WithMessage("Updated author's name is required.")
+            .Length(6, 100).WithMessage("Updated author's name must be between 6 and 100 characters.");
+
+        RuleFor(b => b.PublicationYear)
+            .InclusiveBetween(1000, DateTime.UtcNow.Year)
+            .WithMessage($"Updated publication year must be between 1000 and {DateTime.UtcNow.Year}.");
+    }
 }
